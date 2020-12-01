7742c2628bbb3b8f6a34a0ff581315380d3ea1fb
Dennis Koopman <denniskoopman1@gmail.com>
2020-12-01 06:45:26 +0100

    Refactor day 1

--- AOC20_1.py
+++ AOC20_1.py
@@ -1,35 +1,29 @@
 from helpers import file_reader
 from itertools import combinations
-
-t0_raw = """1721
-979
-366
-299
-675
-1456
-"""
-
-t0 = [x.strip() for x in t0_raw.strip().split('\n')]
-# print(t0)
+import math
 
 
-def compute(data):
-    clean = [int(x) for x in data]
-    combos = combinations(clean, 3)
-    for combo in combos:
-        left, mid, right = combo
-        if left + right + mid == 2020:
-            return left * right * mid
-    return False
+def compute(data, entries):
+    clean_data = [int(x) for x in data]
+    unique_combos = combinations(clean_data, entries)
+    for combo in unique_combos:
+        if sum(combo) == 2020:
+            return math.prod(combo)
+    return None
 
 
-def compute_two(data):
-    pass
+if __name__ == '__main__':
+    t0_raw = """1721
+    979
+    366
+    299
+    675
+    1456
+    """
+    t0 = [x.strip() for x in t0_raw.strip().split('\n')]
+    assert compute(t0, 2) == 514579
+    assert compute(t0, 3) == 241861950
 
-# print(compute(t0))
-
-day_raw = file_reader('2020_1', output='lines')
-# print(day_raw)
-
-print(compute(day_raw))
-
+    day1 = file_reader('2020_1', output='lines')
+    print("solution part 1:", compute(day1, 2))
+    print("solution part 2:", compute(day1, 3))
