8f267d4defef7a1baa11ef23496794b4ce5024b8
Dennis Koopman <denniskoopman1@gmail.com>
2020-12-16 20:35:36 +0100

    Minor changes day 15

--- AOC20_15.py
+++ AOC20_15.py
@@ -5,7 +5,7 @@ from time import perf_counter
 def compute(data: List[int], target: int) -> int:
     """
     Return last spoken number when turn == target.
-    Solution works for both parts, but is very slow for part 2.
+    Solution works for both parts, but is slow for part 2.
     """
     seen = {num: i for i, num in enumerate(data[:-1], start=1)}
     spoken = data[-1]
@@ -23,17 +23,25 @@ def compute(data: List[int], target: int) -> int:
         turn += 1
 
 
-assert compute([0, 3, 6], 2020) == 436
-assert compute([1, 3, 2], 2020) == 1
-assert compute([2, 1, 3], 2020) == 10
-assert compute([1, 2, 3], 2020) == 27
-assert compute([2, 3, 1], 2020) == 78
-assert compute([3, 2, 1], 2020) == 438
-assert compute([3, 1, 2], 2020) == 1836
+if __name__ == "__main__":
+    assert compute([0, 3, 6], 2020) == 436
+    assert compute([1, 3, 2], 2020) == 1
+    assert compute([2, 1, 3], 2020) == 10
+    assert compute([1, 2, 3], 2020) == 27
+    assert compute([2, 3, 1], 2020) == 78
+    assert compute([3, 2, 1], 2020) == 438
+    assert compute([3, 1, 2], 2020) == 1836
 
-day15 = [10, 16, 6, 0, 1, 17]
-st = perf_counter()
-print("solution part 1:", compute(day15, 2020))
-end = perf_counter()
-print(f"runtime: {round((end - st) * 1000, 1)}ms")
-print("Same algorithm will work for part 2, but it will take 90+ seconds")
+    day15 = [10, 16, 6, 0, 1, 17]
+    print("calculating...")
+    st = perf_counter()
+    p1 = compute(day15, 2020)
+    st_p2 = perf_counter()
+    p2 = compute(day15, 30000000)
+    end = perf_counter()
+    t1 = round(st_p2 - st, 2)
+    t2 = round(end - st_p2, 2)
+    total = round(end - st, 2)
+    print(f"solution part 1: {p1}({t1}ms)")
+    print(f"solution part 2: {p2}({t2}ms)")
+    print(f"runtime: {total} seconds")
