45041f22868241bac03124aa46ff08ad57153dd3
Dennis Koopman <denniskoopman1@gmail.com>
2021-01-06 23:06:02 +0100

    Refactor day 10 part 2 to dynamic programming algo

--- AOC20_10.py
+++ AOC20_10.py
@@ -1,10 +1,14 @@
-from helpers import file_reader
 from collections import deque
 from typing import Dict, Deque, List, Set, Tuple, Union
 from time import perf_counter
 
 
-def compute(data: List[int]) -> int:
+def get_input(data_file: str) -> List[int]:
+    with open(data_file) as f:
+        return [int(x) for x in f.readlines()]
+
+
+def compute_p1(data: List[int]) -> int:
     """Answer part 1"""
     adapters = sorted(data)
     device = adapters[-1] + 3
@@ -55,7 +59,7 @@ def compute_two(data: List[int]) -> Union[int, str]:
         # dad = frontier.pop()
         dad = cur_route[-1]
         kids = graph[dad]
-        if kids == 'goal':
+        if kids == "goal":
             routes += 1
             if routes == 1000000:
                 end = perf_counter()
@@ -85,8 +89,6 @@ def compute_new(data: List[int]) -> int:
     4 it is only possible to get to it from 1, so 1 route
     5 it is only possible to get to it from 4, so 1 route
     6 it is possible to get here from 4 or 5, so 2 routes
-    7 it is
-
     """
     adapters = sorted(data)
     graph = build_graph(adapters)
@@ -98,7 +100,7 @@ def compute_new(data: List[int]) -> int:
     while frontier:
         dad = frontier.popleft()
         kids = graph[dad]
-        if kids == 'goal':
+        if kids == "goal":
             continue
         for kid in kids:
             if kid not in all_dads:
@@ -116,28 +118,43 @@ def compute_new(data: List[int]) -> int:
     return routes
 
 
-if __name__ == '__main__':
+def compute_p2(data: List[int]) -> int:
+    """Dynamic programming solution part two."""
+    arrangements = {}
+    adapters = [0] + sorted(data)
+    arrangements[adapters[0]] = 1
+    arrangements[adapters[1]] = 1
+    for i, a in enumerate(adapters[2:], start=2):
+        previous = adapters[max(i - 3, 0) : i]
+        sources = 0
+        for p in previous:
+            if a - p < 4:
+                sources += arrangements[p]
+        arrangements[a] = sources
+    return arrangements[adapters[-1]]
+
+
+if __name__ == "__main__":
     t0 = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
     t1 = [28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19,
           38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3]
 
-    assert compute(t0) == 35
-    assert compute(t1) == 220
-    assert compute_new(t0) == 8
-    assert compute_new(t1) == 19208
-    assert compute_two(t0) == 8
-    assert compute_two(t1) == 19208
+    assert compute_p1(t0) == 35
+    assert compute_p1(t1) == 220
+    assert compute_p2(t0) == 8
+    assert compute_p2(t1) == 19208
 
-    day10_raw = file_reader('inputs/2020_10.txt', output='lines')
-    day10 = [int(x) for x in day10_raw]
-
-    st = perf_counter()
-    p1 = compute(day10)
-    p2 = compute_new(day10)
+    start = perf_counter()
+    day10 = get_input("inputs/2020_10.txt")
+    sp1 = perf_counter()
+    p1 = compute_p1(day10)
+    sp2 = perf_counter()
+    p2 = compute_p2(day10)
     end = perf_counter()
-    tot_time = round((end - st) * 1000, 1)
-    print(f"solution part 1: {p1}")
-    print(f"solution part 2: {p2}")
-    print(f"execution time: {tot_time}ms")
-
-    # print(compute_two(day10))
+    time0 = round((sp1 - start) * 1000, 3)
+    time1 = round((sp2 - sp1) * 1000, 3)
+    time2 = round((end - sp2) * 1000, 3)
+    total_time = round((end - start) * 1000, 3)
+    print(f"solution part 1: {p1} ({time1}ms)")
+    print(f"solution part 2: {p2} ({time2}ms)")
+    print(f"data import took {time0}ms and total runtime is {total_time}ms\n")
